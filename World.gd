84682f93cf5393e2060f64ccc60e8d7aabb87b26
davidalpha <david.alpha@gmail.com>
2021-05-18 20:29:42 +0200

    added ocean+foam

--- World.gd
+++ World.gd
@@ -8,6 +8,8 @@ export var noise_persistence = 0.5
 export var size_depth = 400
 export var size_width = 400
 export var subdivide = 400
+export var steepness = 20
+
 
 func _ready():
 
@@ -34,13 +36,14 @@ func _ready():
 	
 	for i in range (data_tool.get_vertex_count()):
 		var vertex = data_tool.get_vertex(i)
-		vertex.y = noise.get_noise_3d(vertex.x, vertex.y, vertex.z) * 20
+		vertex.y = noise.get_noise_3d(vertex.x, vertex.y, vertex.z) * steepness
 		
 		data_tool.set_vertex(i, vertex)
 		
 	for i in range (array_plane.get_surface_count()):
 		array_plane.surface_remove(i)
 			
+	
 	data_tool.commit_to_surface(array_plane)
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	surface_tool.create_from(array_plane, 0)
