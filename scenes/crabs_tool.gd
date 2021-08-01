ec4474559a72032d0a33b3d46b25bcd99d5c724d
Barend <barend.poot@gmail.com>
2021-08-01 21:40:53 +0200

    rock updated

--- scenes/crabs_tool.gd
+++ scenes/crabs_tool.gd
@@ -4,7 +4,7 @@ extends Spatial
 var amount_trees = 100
 const ray_length = 1000
 var object_type = "rock1"
-var rock1 = preload("res://game_objects/rock1.tscn")
+var rock1 = preload("res://game_objects/rock3.tscn")
 var rock2 = preload("res://game_objects/rock2.tscn")
 
 func _input(event):
@@ -28,6 +28,7 @@ func _input(event):
 					var new_meshinstance = rock1.instance()
 					new_meshinstance.transform.origin = Vector3(placement)
 					new_meshinstance.transform.basis = new_meshinstance.transform.basis.rotated(Vector3(0.0,1.0,0.0), (PI*rand_range(0,2)))
+					new_meshinstance.transform.basis = new_meshinstance.transform.basis.scaled(Vector3(rand_range(.5, 1.5),rand_range(.5, 1.5),rand_range(.5, 1.5)))
 					add_child(new_meshinstance)
 
 				if object_type == "rock2":
@@ -35,4 +36,5 @@ func _input(event):
 					var new_meshinstance = rock2.instance()
 					new_meshinstance.transform.origin = Vector3(placement)
 					new_meshinstance.transform.basis = new_meshinstance.transform.basis.rotated(Vector3(0.0,1.0,0.0), (PI*rand_range(0,2)))
+					new_meshinstance.transform.basis = new_meshinstance.transform.basis.scaled(Vector3(rand_range(.5, 1.5),rand_range(.5, 1.5),rand_range(.5, 1.5)))
 					add_child(new_meshinstance)	
