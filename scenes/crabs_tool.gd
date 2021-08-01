d4eb4dd34f8408951ff138c9bd69fcf7c7a0d2a7
Barend <barend.poot@gmail.com>
2021-08-01 21:59:23 +0200

    randomly choses a rock, UI needs to be updated

--- scenes/crabs_tool.gd
+++ scenes/crabs_tool.gd
@@ -4,8 +4,10 @@ extends Spatial
 var amount_trees = 100
 const ray_length = 1000
 var object_type = "rock1"
-var rock1 = preload("res://game_objects/rock3.tscn")
+var rock1 = preload("res://game_objects/rock1.tscn")
 var rock2 = preload("res://game_objects/rock2.tscn")
+var rock3 = preload("res://game_objects/rock3.tscn")
+var rocks_arr = [rock1, rock2, rock3]
 
 func _input(event):
 
@@ -24,15 +26,16 @@ func _input(event):
 				
 				object_type = UI.object_selected
 				if object_type == "rock1":
-					print(object_type)
-					var new_meshinstance = rock1.instance()
+					rocks_arr.shuffle()
+					var item = rocks_arr[0]
+					var new_meshinstance = item.instance()
 					new_meshinstance.transform.origin = Vector3(placement)
 					new_meshinstance.transform.basis = new_meshinstance.transform.basis.rotated(Vector3(0.0,1.0,0.0), (PI*rand_range(0,2)))
 					new_meshinstance.transform.basis = new_meshinstance.transform.basis.scaled(Vector3(rand_range(.5, 1.5),rand_range(.5, 1.5),rand_range(.5, 1.5)))
 					add_child(new_meshinstance)
 
 				if object_type == "rock2":
-					print(object_type)
+					print("DEZE OPRUIMEN")
 					var new_meshinstance = rock2.instance()
 					new_meshinstance.transform.origin = Vector3(placement)
 					new_meshinstance.transform.basis = new_meshinstance.transform.basis.rotated(Vector3(0.0,1.0,0.0), (PI*rand_range(0,2)))
